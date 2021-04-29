7faa9dd0a611fda3d0a12a471e2a75954abcb0b1
Umair Jibran <umairjibran7@gmail.com>
2021-04-29 08:59:07 +0500

    add height/width functions and appbar

--- lib/src/views/v_student_dashboard.dart
+++ lib/src/views/v_student_dashboard.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 class StudentDashboard extends StatelessWidget {
   static final routeName = "/student-dashboard";
 
+  /*Functions*/
+  getHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
+  getWidth(BuildContext context) => MediaQuery.of(context).size.width;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Student Dashboard"),
+      ),
       body: Container(
+        height: getHeight(context),
+        width: getWidth(context),
         child: Center(
           child: Text("Student Dashboard"),
         ),
